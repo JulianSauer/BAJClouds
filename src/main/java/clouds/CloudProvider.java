9af0b98f71e7ef275e5c85f241fbf773a5845d5d
Julian Sauer <julian_sauer@gmx.net>
2016-08-20 02:18:55 +0200

    Fixed timeout when waiting for console output

--- src/main/java/clouds/CloudProvider.java
+++ src/main/java/clouds/CloudProvider.java
@@ -24,9 +24,9 @@ public abstract class CloudProvider {
 
     protected final String initScript = "mkdir /home/logs" +
             "&& apt-get update" +
-            "&& apt-get install maven git openjdk-7-jdk -y > /home/logs/install.txt" +
-            "&& git clone https://github.com/ewolff/user-registration.git /home/app/ > /home/logs/git.txt" +
-            "&& mvn -f /home/app/user-registration-application/pom.xml spring-boot:run > /home/logs/mvn.txt";
+            "&& apt-get install maven git openjdk-7-jdk -y" +
+            "&& git clone https://github.com/ewolff/user-registration.git /home/app/" +
+            "&& mvn -f /home/app/user-registration-application/pom.xml spring-boot:run &";
 
     public CloudProvider(String userKey, String passwordKey, String provider) {
         Accounts accounts = new Accounts();
